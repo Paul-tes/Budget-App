b5199ccab499d60e67577c341809d71ff7662afc
paul-tes <pauasca8462@gmail.com>
2023-09-12 22:48:49 +0300

    Add test for catefory and purchasse model

---
+++ db/migrate/20230912132710_create_categories_purchases.rb
@@ -0,0 +1,11 @@
+class CreateCategoriesPurchases < ActiveRecord::Migration[7.0]
+  def change
+    create_table :categories_purchases do |t|
+      t.string :categories_purchases
+      t.references :category, null: false, foreign_key: true
+      t.references :purchase, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/models/purchase_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Purchase, type: :model do
+  it { should belong_to(:user) }
+  it { should have_and_belong_to_many(:categories) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_numericality_of(:amount).is_greater_than_or_equal_to(0) }
+end
