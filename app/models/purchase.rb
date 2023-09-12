48c5375587aa72ae990555065a05a35312e02432
paul-tes <pauasca8462@gmail.com>
2023-09-12 22:48:28 +0300

    Fix relationships

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,6 +2,6 @@ class Purchase < ApplicationRecord
   validates :name, presence: true
   validates :amount, presence: true, numericality: { money: true, greater_than_or_equal_to: 0 }
 
-  belongs_to :user
+  belongs_to :user, foreign_key: :author_id, dependent: :destroy
   has_and_belongs_to_many :categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_12_114730) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_132710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_114730) do
     t.bigint "purchase_id", null: false
     t.index ["purchase_id"], name: "index_categories_on_purchase_id"
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_purchases", force: :cascade do |t|
+    t.string "categories_purchases"
+    t.bigint "category_id", null: false
+    t.bigint "purchase_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categories_purchases_on_category_id"
+    t.index ["purchase_id"], name: "index_categories_purchases_on_purchase_id"
   end
 
   create_table "purchases", force: :cascade do |t|
@@ -44,6 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_114730) do
 
   add_foreign_key "categories", "purchases"
   add_foreign_key "categories", "users"
+  add_foreign_key "categories_purchases", "categories"
+  add_foreign_key "categories_purchases", "purchases"
   add_foreign_key "purchases", "categories"
   add_foreign_key "purchases", "users", column: "author_id"
 end
