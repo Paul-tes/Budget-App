747f3b0d9ca1defd0a39f855e2797d5469b45e3a
paul-tes <pauasca8462@gmail.com>
2023-09-12 15:22:50 +0300

    Add validations

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,7 @@
 class Purchase < ApplicationRecord
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { money: true, greater_than_or_equal_to: 0 }
+
   belongs_to :user
   has_and_belongs_to_many :catagories
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+
   has_many :categories
   has_many :purchases
 end
