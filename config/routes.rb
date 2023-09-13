7dd832f942bfe5b4373674468889d50091f76a98
paul-tes <pauasca8462@gmail.com>
2023-09-13 17:20:49 +0300

    Add proper routes for a devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root :to => 'splash_screen#index'
+  devise_for :users, controllers: {registrations: "registrations"}
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'categories#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'splash_screen#index', as: :unauthenticated_root
+    end
+  end
   resources :users
   resources :categories do
     resources :purchases
