05d5524a2872650cdbd3b5e899c1b1b845c59896
paul-tes <pauasca8462@gmail.com>
2023-09-13 15:07:58 +0300

    Add user actions and thire templtes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'splash_screen#index'
   resources :users
-  resources :categories
-  resources :purchases
-  resources :splash_screen
+  resources :categories do
+    resources :purchases
+  end
 end
