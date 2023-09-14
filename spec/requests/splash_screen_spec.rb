a9f58335021a203c80c420f320a2bb3ce963069d
paul-tes <pauasca8462@gmail.com>
2023-09-14 13:51:13 +0300

    Add request test + fix rubocop

--- spec/requests/splash_screen_spec.rb
+++ spec/requests/splash_screen_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "SplashScreens", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'SplashScreen', type: :request do
+  describe 'GET /' do
+    it 'renders the home page with the correct content' do
+      get unauthenticated_root_path
+      expect(response).to have_http_status(:success)
+    end
   end
 end
